1d296ebaef321b22e103a387e9363224d1967c7a
ElderBobr <entoakk20@gmail.com>
2023-06-02 01:11:38 +0700

    Добавьте файлы проекта.

---
+++ Multiples_of_3_or_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int Multiples(int a) {
+    int sum = 0;
+    for (int i = 0; i < a; i++) {
+        if (a % 3 == 0 || a % 5 == 0) {
+            sum += i;
+        }
+    }
+    cout << sum;
+    return sum;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int Number = 0;
+    cin >> Number;
+    if (Number < 0) {
+        cout << "Введи правильное число";
+        exit(0);
+    }
+    Multiples(Number);
+}
